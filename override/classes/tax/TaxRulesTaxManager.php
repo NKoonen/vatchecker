11e0e47a09ee4cb18a28c13eaa5b7a317f8d4dda
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2020-09-29 14:57:09 +0200

    Reuse the parent class code + indentation fix

--- override/classes/tax/TaxRulesTaxManager.php
+++ override/classes/tax/TaxRulesTaxManager.php
@@ -30,109 +30,28 @@ use PrestaShop\PrestaShop\Adapter\ServiceLocator;
  */
 class TaxRulesTaxManager extends TaxRulesTaxManagerCore
 {
-    public $address;
-    public $type;
-    public $tax_calculator;
+	/**
+	 * Return the tax calculator associated to this address.
+	 *
+	 * @return TaxCalculator
+	 */
+	public function getTaxCalculator()
+	{
+		static $tax_enabled = null;
 
-    /**
-     * @var \PrestaShop\PrestaShop\Core\ConfigurationInterface
-     */
-    private $configurationManager;
+		if (isset($this->tax_calculator)) {
+			return $this->tax_calculator;
+		}
 
-    /**
-     * @param Address $address
-     * @param mixed $type An additional parameter for the tax manager (ex: tax rules id for TaxRuleTaxManager)
-     */
-    public function __construct(Address $address, $type, PrestaShop\PrestaShop\Core\ConfigurationInterface $configurationManager = null)
-    {
-        if ($configurationManager === null) {
-            $this->configurationManager = ServiceLocator::get('\\PrestaShop\\PrestaShop\\Core\\ConfigurationInterface');
-        } else {
-            $this->configurationManager = $configurationManager;
-        }
+		if ($tax_enabled === null) {
+			#The check if customer is in the Tax free group
+			$tax_enabled = Configuration::get('PS_TAX') && !in_array(Configuration::get('VATCHECKER_NO_TAX_GROUP'), Customer::getGroupsStatic($this->address->id_customer));
+		}
 
-        $this->address = $address;
-        $this->type = $type;
-    }
+		if (!$tax_enabled) {
+			return new TaxCalculator(array());
+		}
 
-    /**
-     * Returns true if this tax manager is available for this address.
-     *
-     * @return bool
-     */
-    public static function isAvailableForThisAddress(Address $address)
-    {
-        return true; // default manager, available for all addresses
-    }
-
-    /**
-     * Return the tax calculator associated to this address.
-     *
-     * @return TaxCalculator
-     */
-    public function getTaxCalculator()
-    {
-        static $tax_enabled = null;
-
-        if (isset($this->tax_calculator)) {
-            return $this->tax_calculator;
-        }
-
-        if ($tax_enabled === null) {
-            #The check if customer is in the Tax free group
-            $tax_enabled = $this->configurationManager->get('PS_TAX') && !in_array(Configuration::get('VATCHECKER_NO_TAX_GROUP'), Customer::getGroupsStatic($this->address->id_customer));
-        }
-
-        if (!$tax_enabled) {
-            return new TaxCalculator(array());
-        }
-
-        $taxes = array();
-        $postcode = 0;
-
-        if (!empty($this->address->postcode)) {
-            $postcode = $this->address->postcode;
-        }
-
-        $cache_id = (int) $this->address->id_country . '-' . (int) $this->address->id_state . '-' . $postcode . '-' . (int) $this->type;
-
-        if (!Cache::isStored($cache_id)) {
-            $rows = Db::getInstance()->executeS('
-				SELECT tr.*
-				FROM `' . _DB_PREFIX_ . 'tax_rule` tr
-				JOIN `' . _DB_PREFIX_ . 'tax_rules_group` trg ON (tr.`id_tax_rules_group` = trg.`id_tax_rules_group`)
-				WHERE trg.`active` = 1
-				AND tr.`id_country` = ' . (int) $this->address->id_country . '
-				AND tr.`id_tax_rules_group` = ' . (int) $this->type . '
-				AND tr.`id_state` IN (0, ' . (int) $this->address->id_state . ')
-				AND (\'' . pSQL($postcode) . '\' BETWEEN tr.`zipcode_from` AND tr.`zipcode_to`
-					OR (tr.`zipcode_to` = 0 AND tr.`zipcode_from` IN(0, \'' . pSQL($postcode) . '\')))
-				ORDER BY tr.`zipcode_from` DESC, tr.`zipcode_to` DESC, tr.`id_state` DESC, tr.`id_country` DESC');
-
-            $behavior = 0;
-            $first_row = true;
-
-            foreach ($rows as $row) {
-                $tax = new Tax((int) $row['id_tax']);
-
-                $taxes[] = $tax;
-
-                // the applied behavior correspond to the most specific rules
-                if ($first_row) {
-                    $behavior = $row['behavior'];
-                    $first_row = false;
-                }
-
-                if ($row['behavior'] == 0) {
-                    break;
-                }
-            }
-            $result = new TaxCalculator($taxes, $behavior);
-            Cache::store($cache_id, $result);
-
-            return $result;
-        }
-
-        return Cache::retrieve($cache_id);
-    }
+		return parent::getTaxCalculator();
+	}
 }
