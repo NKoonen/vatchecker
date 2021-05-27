54e7bd5ef51a40b7a2ea83de912fba0976b23f8f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-05-27 13:30:03 +0200

    AJAX VAT validator

--- views/js/front.js
+++ views/js/front.js
@@ -28,11 +28,56 @@
 
 jQuery( function( $ ) {
 
+	vatchecker.validate = function( vat_number, id_country, $elem ) {
+		const token = prestashop.static_token;
+		$elem.removeClass( 'validated error text-danger text-success' ).css( { 'opacity': '0.5' } );
+		$elem.next( '.vat-error' ).remove();
+
+		$.ajax( {
+			type: 'POST',
+			url: vatchecker.ajax_url,
+			headers: {"cache-control": "no-cache"},
+			async: false,
+			data: {
+				vatchecker: token,
+				vat_number: vat_number,
+				id_country: id_country,
+			},
+			dataType: 'json',
+			success: function ( resp ) {
+				if ( resp.hasOwnProperty( 'valid' ) ) {
+					// Check successful.
+					if ( true === resp.valid ) {
+						// Valid VAT
+						$elem.addClass( 'validated text-success' );
+					} else {
+						$elem.addClass( 'error text-danger' );
+						if ( resp.hasOwnProperty( 'error' ) && resp.error ) {
+							// Error message.
+							$elem.after( '<p class="vat-error small text-danger">' + resp.error + '</p>' );
+						}
+					}
+				} else {
+					// Fail
+					$elem.addClass( 'error text-danger' );
+				}
+			}
+		} ).always( function() {
+			$elem.css( { 'opacity': '' } );
+		} ).fail( function( resp ) {
+			$elem.addClass( 'error text-danger' );
+		} );
+	};
+
 	$(document).on( 'blur', '[name="vat_number"]', function () {
 		var $this = $( this ),
+			$form = $this.parents( 'form' ),
+			$country = $form.find('[name="id_country"]');
 
 		// Remove invalid characters.
 		$this.val( $this.val().toUpperCase().replace( /[^A-Z0-9]/gi, '' ) );
+
+		vatchecker.validate( $( this ).val(), $country.val(), $this );
 	} );
 
 } );
