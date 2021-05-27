4b15f2f7eb5d6d0ad040c4b36b33b19dc015b65a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-05-27 13:28:33 +0200

    Convert indentation to tabs

--- vatchecker.php
+++ vatchecker.php
@@ -25,243 +25,243 @@
  */
 
 if (!defined('_PS_VERSION_')) {
-    exit;
+	exit;
 }
 
 class Vatchecker extends Module
 {
-    protected $config_form = false;
-    private $_SOAPUrl = 'https://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
-    private $EUCountries = array(
-        'AT',
-        'BE',
-        'BG',
-        'CY',
-        'CZ',
-        'DE',
-        'DK',
-        'EE',
-        'GR',
-        'ES',
-        'FI',
-        'FR',
-        'GB',
-        'HR',
-        'HU',
-        'IE',
-        'IT',
-        'LT',
-        'LU',
-        'LV',
-        'MT',
-        'NL',
-        'PL',
-        'PT',
-        'RO',
-        'SE',
-        'SI',
-        'SK',
-    );
+	protected $config_form = false;
+	private $_SOAPUrl = 'https://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+	private $EUCountries = array(
+		'AT',
+		'BE',
+		'BG',
+		'CY',
+		'CZ',
+		'DE',
+		'DK',
+		'EE',
+		'GR',
+		'ES',
+		'FI',
+		'FR',
+		'GB',
+		'HR',
+		'HU',
+		'IE',
+		'IT',
+		'LT',
+		'LU',
+		'LV',
+		'MT',
+		'NL',
+		'PL',
+		'PT',
+		'RO',
+		'SE',
+		'SI',
+		'SK',
+	);
 
-    public function __construct()
-    {
-        $this->name = 'vatchecker';
-        $this->tab = 'billing_invoicing';
-        $this->version = '1.0.2';
-        $this->author = 'Inform-All';
-        $this->need_instance = 1;
+	public function __construct()
+	{
+		$this->name = 'vatchecker';
+		$this->tab = 'billing_invoicing';
+		$this->version = '1.0.0';
+		$this->author = 'Inform-All';
+		$this->need_instance = 1;
 
-        /**
-         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-         */
-        $this->bootstrap = true;
+		/**
+		 * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+		 */
+		$this->bootstrap = true;
 
-        parent::__construct();
+		parent::__construct();
 
-        $this->displayName = $this->l('Vat Checker');
-        $this->description = $this->l(
-            'Check if a customers VAT number is valid and gives the customer 0 tax if the customer is not in from country.'
-        );
+		$this->displayName = $this->l('Vat Checker');
+		$this->description = $this->l(
+			'Check if a customers VAT number is valid and gives the customer 0 tax if the customer is not in from country.'
+		);
 
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
-    }
+		$this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+	}
 
-    /**
-     * Don't forget to create update methods if needed:
-     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
-     */
-    public function install()
-    {
-        Configuration::updateValue('VATCHECKER_LIVE_MODE', true);
+	/**
+	 * Don't forget to create update methods if needed:
+	 * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+	 */
+	public function install()
+	{
+		Configuration::updateValue('VATCHECKER_LIVE_MODE', true);
 
-        return parent::install() &&
+		return parent::install() &&
 			$this->registerHook('displayHeader') &&
-            $this->registerHook('actionValidateCustomerAddressForm');
-    }
+			$this->registerHook('actionValidateCustomerAddressForm');
+	}
+
+	public function uninstall()
+	{
+		Configuration::deleteByName('VATCHECKER_LIVE_MODE');
+
+		return parent::uninstall();
+	}
 
 	public function hookDisplayHeader( $params )
 	{
 		$this->context->controller->addJS( $this->_path . 'views/js/front.js' );
 	}
 
-    public function uninstall()
-    {
-        Configuration::deleteByName('VATCHECKER_LIVE_MODE');
+	/**
+	 * Load the configuration form
+	 */
+	public function getContent()
+	{
+		/**
+		 * If values have been submitted in the form, process.
+		 */
+		if (((bool)Tools::isSubmit('submitVatcheckerModule')) == true) {
+			$this->postProcess();
+		}
 
-        return parent::uninstall();
-    }
+		$this->context->smarty->assign('module_dir', $this->_path);
 
-    /**
-     * Load the configuration form
-     */
-    public function getContent()
-    {
-        /**
-         * If values have been submitted in the form, process.
-         */
-        if (((bool)Tools::isSubmit('submitVatcheckerModule')) == true) {
-            $this->postProcess();
-        }
+		$output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+		$output = $output.$this->renderForm();
 
-        $this->context->smarty->assign('module_dir', $this->_path);
+		return $output;
+	}
 
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
-        $output = $output.$this->renderForm();
+	/**
+	 * Save form data.
+	 */
+	protected function postProcess()
+	{
+		$form_values = $this->getConfigFormValues();
 
-        return $output;
-    }
+		foreach (array_keys($form_values) as $key) {
+			Configuration::updateValue($key, Tools::getValue($key));
+		}
+	}
 
-    /**
-     * Save form data.
-     */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
+	/**
+	 * Set values for the inputs.
+	 */
+	protected function getConfigFormValues()
+	{
+		return array(
+			'VATCHECKER_LIVE_MODE' => Configuration::get('VATCHECKER_LIVE_MODE', true),
+			'VATCHECKER_ORIGIN_COUNTRY' => Configuration::get('VATCHECKER_ORIGIN_COUNTRY', '0'),
+			'VATCHECKER_NO_TAX_GROUP' => Configuration::get('VATCHECKER_NO_TAX_GROUP', null),
+		);
+	}
 
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
-        }
-    }
+	/**
+	 * Create the form that will be displayed in the configuration of your module.
+	 */
+	protected function renderForm()
+	{
+		$helper = new HelperForm();
 
-    /**
-     * Set values for the inputs.
-     */
-    protected function getConfigFormValues()
-    {
-        return array(
-            'VATCHECKER_LIVE_MODE' => Configuration::get('VATCHECKER_LIVE_MODE', true),
-            'VATCHECKER_ORIGIN_COUNTRY' => Configuration::get('VATCHECKER_ORIGIN_COUNTRY', '0'),
-            'VATCHECKER_NO_TAX_GROUP' => Configuration::get('VATCHECKER_NO_TAX_GROUP', null),
-        );
-    }
+		$helper->show_toolbar = false;
+		$helper->table = $this->table;
+		$helper->module = $this;
+		$helper->default_form_language = $this->context->language->id;
+		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
 
-    /**
-     * Create the form that will be displayed in the configuration of your module.
-     */
-    protected function renderForm()
-    {
-        $helper = new HelperForm();
+		$helper->identifier = $this->identifier;
+		$helper->submit_action = 'submitVatcheckerModule';
+		$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+			.'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
 
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->module = $this;
-        $helper->default_form_language = $this->context->language->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+		$helper->tpl_vars = array(
+			'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+			'languages' => $this->context->controller->getLanguages(),
+			'id_language' => $this->context->language->id,
+		);
 
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitVatcheckerModule';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
+		return $helper->generateForm(array($this->getConfigForm()));
+	}
 
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
+	/**
+	 * Create the structure of your form.
+	 */
+	protected function getConfigForm()
+	{
+		$countries = Country::getCountries($this->context->language->id);
+		$cntylist = array(
+			0 => array(
+				'id' => 0,
+				'name' => $this->l('- Select a country -'),
+			),
+		);
+		foreach ($countries as $country) {
+			$cntylist[] = array(
+				'id' => $country['id_country'],
+				'name' => $country['name'],
+			);
+		}
 
-        return $helper->generateForm(array($this->getConfigForm()));
-    }
-
-    /**
-     * Create the structure of your form.
-     */
-    protected function getConfigForm()
-    {
-        $countries = Country::getCountries($this->context->language->id);
-        $cntylist = array(
-            0 => array(
-                'id' => 0,
-                'name' => $this->l('- Select a country -'),
-            ),
-        );
-        foreach ($countries as $country) {
-            $cntylist[] = array(
-                'id' => $country['id_country'],
-                'name' => $country['name'],
-            );
-        }
-
-        return array(
-            'form' => array(
-                'legend' => array(
-                    'title' => $this->l('Settings'),
-                    'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->l('Activate module'),
-                        'name' => 'VATCHECKER_LIVE_MODE',
-                        'is_bool' => true,
-                        'desc' => $this->l('Use this module currently active'),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => true,
-                                'label' => $this->l('Enabled'),
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => false,
-                                'label' => $this->l('Disabled'),
-                            ),
-                        ),
-                    ),
-                    array(
-                        'col' => 3,
-                        'type' => 'select',
-                        'desc' => $this->l('Select shops country'),
-                        'name' => 'VATCHECKER_ORIGIN_COUNTRY',
-                        'label' => $this->l('Origin'),
-                        'options' => array(
-                            'query' => $cntylist,
-                            'id' => 'id',
-                            'name' => 'name',
-                        ),
-                    ),
-                    array(
-                        'type' => 'select',
-                        'name' => 'VATCHECKER_NO_TAX_GROUP',
-                        'label' => $this->l('Valid VAT Group'),
-                        'desc' => $this->l('Customers with valid VAT number will be place in this group.'),
-                        'options' => array(
-                            'query' => Group::getGroups(Context::getContext()->language->id, true),
-                            'id' => 'id_group',
-                            'name'=>'name',
-                            'default' => array(
-                                'value' => '',
-                                'label' => $this->l('Select Group')
-                            )
-                        )
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                ),
-            ),
-        );
-    }
+		return array(
+			'form' => array(
+				'legend' => array(
+					'title' => $this->l('Settings'),
+					'icon' => 'icon-cogs',
+				),
+				'input' => array(
+					array(
+						'type' => 'switch',
+						'label' => $this->l('Activate module'),
+						'name' => 'VATCHECKER_LIVE_MODE',
+						'is_bool' => true,
+						'desc' => $this->l('Use this module currently active'),
+						'values' => array(
+							array(
+								'id' => 'active_on',
+								'value' => true,
+								'label' => $this->l('Enabled'),
+							),
+							array(
+								'id' => 'active_off',
+								'value' => false,
+								'label' => $this->l('Disabled'),
+							),
+						),
+					),
+					array(
+						'col' => 3,
+						'type' => 'select',
+						'desc' => $this->l('Select shops country'),
+						'name' => 'VATCHECKER_ORIGIN_COUNTRY',
+						'label' => $this->l('Origin'),
+						'options' => array(
+							'query' => $cntylist,
+							'id' => 'id',
+							'name' => 'name',
+						),
+					),
+					array(
+						'type' => 'select',
+						'name' => 'VATCHECKER_NO_TAX_GROUP',
+						'label' => $this->l('Valid VAT Group'),
+						'desc' => $this->l('Customers with valid VAT number will be place in this group.'),
+						'options' => array(
+							'query' => Group::getGroups(Context::getContext()->language->id, true),
+							'id' => 'id_group',
+							'name'=>'name',
+							'default' => array(
+								'value' => '',
+								'label' => $this->l('Select Group')
+							)
+						)
+					),
+				),
+				'submit' => array(
+					'title' => $this->l('Save'),
+				),
+			),
+		);
+	}
 
 	public function hookActionValidateCustomerAddressForm(&$params)
 	{
