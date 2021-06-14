c039e5f8d32cfc18a3369bb235db5a51f886d80a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-06-14 10:33:00 +0200

    v1.2.1

--- vatchecker.php
+++ vatchecker.php
@@ -67,7 +67,7 @@ class Vatchecker extends Module
 	{
 		$this->name = 'vatchecker';
 		$this->tab = 'billing_invoicing';
-		$this->version = '1.2';
+		$this->version = '1.2.1';
 		$this->author = 'Inform-All';
 		$this->need_instance = 1;
 
