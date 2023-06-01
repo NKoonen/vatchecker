2dcc339c0f1c4ea86b686f39db89bd72dfdee427
Inform-All <NKoonen@users.noreply.github.com>
2023-06-01 10:45:28 +0200

    Create es.php

---
+++ translations/es.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{vatchecker}prestashop>vatchecker_48632718301f88aa265f71cdebc9cb45'] = 'Seleccione un país de la UE';
+$_MODULE['<{vatchecker}prestashop>vatchecker_1e1a3efdfb933ee704fa6fb3c82723e5'] = 'Proporcione un número de IVA';
+$_MODULE['<{vatchecker}prestashop>vatchecker_59777e0761f661b4683759f870443e77'] = 'Formato de número de IVA no válido';
+$_MODULE['<{vatchecker}prestashop>vatchecker_8c53573542f636780754a08bdaf81442'] = 'Este no es un número de IVA reconocido';
+$_MODULE['<{vatchecker}prestashop>vatchecker_1c777bdd56e6da3b27bf1f6ddf6d64eb'] = 'El servidor EU VIES no responde';
