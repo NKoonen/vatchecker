77459e27e7157163fab3a3b906c0f0924d4f6720
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-03-07 12:34:02 +0100

    Fix version numbering
    v3 was overwritten

--- vatchecker.php
+++ vatchecker.php
@@ -94,7 +94,7 @@ class Vatchecker extends Module
 	{
 		$this->name          = 'vatchecker';
 		$this->tab           = 'billing_invoicing';
-		$this->version       = '2.1.3';
+		$this->version       = '3.0.1';
 		$this->author        = 'Inform-All & Keraweb';
 		$this->need_instance = 1;
 
